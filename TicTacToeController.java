343420f016c8cc82574390065cda2ce709111e92
lDirtyDan <54995953+lDirtyDan@users.noreply.github.com>
2019-09-29 11:42:59 -0500

    Add files via upload
    Final Push!

---
+++ TicTacToeController.java
@@ -0,0 +1,50 @@
+package edu.jsu.mcis;
+
+public class TicTacToeController {
+
+    private final TicTacToeModel model;
+    private final TicTacToeView view;
+    
+    /* CONSTRUCTOR */
+
+    public TicTacToeController(int width) {
+        
+        /* Initialize model, view, and width */
+
+        model = new TicTacToeModel(width);
+        view = new TicTacToeView();
+        
+    }
+
+    public void start() {
+    
+        /* MAIN LOOP (repeats until game is over) */
+
+        /* Display the board using the View's "showBoard()", then use
+           "getNextMove()" to get the next move from the player.  Enter
+           the move (using the Model's "makeMark()", or display an error
+           using the View's "showInputError()" if the move is invalid. */
+
+           while (! (model.isGameover())) {
+            view.showBoard(model.toString());
+            TicTacToeMove x = view.getNextMove(true);
+
+            boolean result = model.makeMark(x.getRow(),x.getCol());
+
+            if (result) {
+                view.showBoard(model.toString());
+            } 
+            
+            else {
+                view.showInputError();
+            }
+        }
+        /* After the game is over, show the final board and the winner */
+
+        view.showBoard(model.toString());
+
+        view.showResult(model.getResult().toString());
+        
+    }
+
+}
---
+++ TicTacToeView.java
@@ -0,0 +1,53 @@
+package edu.jsu.mcis;
+
+import java.util.Scanner;
+
+public class TicTacToeView {
+    
+    private final Scanner keyboard;
+    
+    /* CONSTRUCTOR */
+	
+    public TicTacToeView() {
+        
+        /* Initialize scanner (for console keyboard) */
+        
+        keyboard = new Scanner(System.in);
+        
+    }
+	
+    public TicTacToeMove getNextMove(boolean isXTurn) {
+        
+        /* Prompt the player to enter the row and the column of their next move.
+           Return as a TicTacToeMove object. */
+        
+        System.out.println("please enter your row then column seperated by a space to make your next move!");
+       int x = keyboard.nextInt();
+       int y = keyboard.nextInt();
+       TicTacToeMove newmove = new TicTacToeMove(x,y);
+        
+
+
+        return newmove;
+
+    }
+
+    public void showInputError() {
+
+        System.out.println("Entered location is invalid, already marked, or out of bounds.");
+
+    }
+
+    public void showResult(String r) {
+
+        System.out.println(r + "!");
+
+    }
+    
+    public void showBoard(String board) {
+        
+        System.out.println("\n\n" + board);
+        
+    }
+	
+}
